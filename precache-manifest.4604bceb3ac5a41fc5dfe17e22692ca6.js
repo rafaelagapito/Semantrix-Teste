7cd82b80bd3168e96982f784f6ef929707114e9d
rafaelagapito <tonhogott22@hotmail.com>
2019-07-02 16:40:21 -0300

    First

---
+++ precache-manifest.4604bceb3ac5a41fc5dfe17e22692ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8c5daebfb72d59289d94017a40bbd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "358fde97f368969dfbd7",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "64f7029f2ff936617a5c",
+    "url": "./static/css/main.527e84f2.chunk.css"
+  },
+  {
+    "revision": "358fde97f368969dfbd7",
+    "url": "./static/js/2.3d7bb1f5.chunk.js"
+  },
+  {
+    "revision": "64f7029f2ff936617a5c",
+    "url": "./static/js/main.7b220a69.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
